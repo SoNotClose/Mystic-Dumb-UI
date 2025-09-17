3e25bfd45fda7bd9e184777ddca6082383ad2666
SoNotClose <166761762+SoNotClose@users.noreply.github.com>
2025-09-17 00:59:48 -0400

    Update MysticMain.lua

--- MysticRobloxUi/MysticLib/MysticMain.lua
+++ MysticRobloxUi/MysticLib/MysticMain.lua
@@ -27,6 +27,7 @@ for _, e in ipairs(changelog) do
    newchangelog = newchangelog .. "- " .. e .. "\n"
 end
 
-local Changelog = MysticUI.MainTab:CreateLabel(newchangelog, "megaphone", Color3.fromRGB(120, 120, 120), false)
+-- local Changelog = MysticUI.MainTab:CreateLabel(newchangelog, "megaphone", Color3.fromRGB(120, 120, 120), false)
+
 
 
