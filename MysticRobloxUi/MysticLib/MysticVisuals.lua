c64206fa66a0b0df8b23ee95126e4a68e140b12a
SoNotClose <166761762+SoNotClose@users.noreply.github.com>
2025-09-17 00:57:52 -0400

    useing loadstring instead of reqiure

--- MysticRobloxUi/MysticLib/MysticVisuals.lua
+++ MysticRobloxUi/MysticLib/MysticVisuals.lua
@@ -1,6 +1,7 @@
 -- idk how to make esp or anything lolololol
 
 ---@diagnostic disable: undefined-global
-local MysticUI = require("MysticUI")
+local MysticUI = loadstring(game:HttpGet("https://raw.githubusercontent.com/SoNotClose/Mystic-Dumb-UI/main/MysticRobloxUi/MysticLib/MysticUI.lua"))()
 local visual = MysticUI.VisualTab
 local vSection = visual:CreateSection("Visuals")
+
