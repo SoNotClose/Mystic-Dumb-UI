99bb155575f21567bf43074a561dd8a03e8c3b98
SoNotClose <166761762+SoNotClose@users.noreply.github.com>
2025-09-17 01:01:02 -0400

    Update Mystic.lua

--- MysticRobloxUi/Mystic.lua
+++ MysticRobloxUi/Mystic.lua
@@ -1,6 +1,5 @@
 print("mystic by sonotclose")
 
-loadstring(game:HttpGet("https://raw.githubusercontent.com/SoNotClose/Mystic-Dumb-UI/main/MysticRobloxUi/MysticLib/MysticUI.lua"))()
 loadstring(game:HttpGet("https://raw.githubusercontent.com/SoNotClose/Mystic-Dumb-UI/main/MysticRobloxUi/MysticLib/MysticMain.lua"))()
 loadstring(game:HttpGet("https://raw.githubusercontent.com/SoNotClose/Mystic-Dumb-UI/main/MysticRobloxUi/MysticLib/MysticPlayer.lua"))()
 loadstring(game:HttpGet("https://raw.githubusercontent.com/SoNotClose/Mystic-Dumb-UI/main/MysticRobloxUi/MysticLib/MysticFun.lua"))()
